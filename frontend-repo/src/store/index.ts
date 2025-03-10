1234aa2fc4ecca3ee6aacab37107a2f8a3e5e4e9
Kelvin <74870436+JRSKelvin@users.noreply.github.com>
2025-03-10 22:18:31 +0700

    Added User Slice For Redux

--- frontend-repo/src/store/index.ts
+++ frontend-repo/src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
 import authReducer from "./slices/authSlice";
+import userReducer from "./slices/userSlice";
 
 export const store = configureStore({
   reducer: {
+    user: userReducer,
     auth: authReducer,
   },
 });
---
+++ frontend-repo/src/store/slices/userSlice.ts
@@ -0,0 +1,53 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axiosInstance from "@/services/request";
+
+interface UserState {
+  loading: boolean | undefined;
+  error: object | undefined;
+  data: object | undefined;
+}
+
+const initialState: UserState = {
+  loading: undefined,
+  error: undefined,
+  data: undefined,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.data = action.payload;
+      })
+      .addCase(getUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error;
+        state.data = undefined;
+      })
+      .addCase(getUser.pending, (state) => {
+        state.loading = true;
+        state.error = undefined;
+        state.data = undefined;
+      })
+  },
+});
+
+export const getUser = createAsyncThunk("user/getUser", async (_, thunkAPI) => {
+  try {
+    const response = await axiosInstance.post(`/api/users/fetch-user-data`);
+    return response.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      return thunkAPI.rejectWithValue(error.message || "Get User Failed");
+    }
+    return thunkAPI.rejectWithValue("An Unknown Error Occurred");
+  }
+});
+
+export default userSlice.reducer;
